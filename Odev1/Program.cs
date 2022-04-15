7335897e33b68290c836951d94c5fbe26d75f650
Sema <Sema@DESKTOP-F3KELN6>
2022-04-15 16:53:48 +0300

    Add project files.

---
+++ Odev1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //BİLGİSAYARIN RASTGELE TUTTUĞU 10 SAYIDAN TEK VE ÇİFT OLANLARIN SAYISI
+            int sayi, tekAdet = 0, ciftAdet = 0;
+            Random rnd = new Random();
+            for (int i = 1; i <= 10; i++)
+            {
+                sayi = rnd.Next(1, 100);
+                Console.WriteLine(sayi);
+                if (sayi % 2 == 0)
+                {
+                    ciftAdet++;
+                }
+                else
+                {
+                    tekAdet++;
+                }
+            }
+            Console.WriteLine("Çift Sayı Adeti: {0}", ciftAdet);
+            Console.WriteLine("Tek Sayı Adeti: {0}", tekAdet);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Odev2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Class Çağırma
+            Urun urun1 = new Urun();
+            urun1.Cinsiyet = "Kadın";
+            urun1.Marka = "Zara";
+            urun1.Kategori = "Gömlek";
+            urun1.Beden = "S";
+            urun1.Fiyat = 499.99;
+
+            Urun urun2 = new Urun();
+            urun2.Cinsiyet = "Kadın";
+            urun2.Marka = "Mango";
+            urun2.Kategori = "Etek";
+            urun2.Beden = "XL";
+            urun2.Fiyat = 700;
+
+            Urun[] urunler = new Urun[]
+            {
+                urun1,
+                urun2
+            };
+
+            /*
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(urun.Cinsiyet + " " + urun.Marka + " " + urun.Kategori + " " + urun.Beden + " Beden " + urun.Fiyat + " TL ");
+            }
+            */
+
+            for (int i = 0; i < urunler.Length; i++)
+            {
+               
+                Console.WriteLine(urunler[i].Cinsiyet + " " + urunler[i].Marka + " " + urunler[i].Kategori + " " + urunler[i].Beden + " Beden " + urunler[i].Fiyat + " TL ");
+            }
+
+            Console.ReadLine();
+
+        }
+        class Urun
+        {
+            public string Cinsiyet { get; set; }    
+            public string Marka { get; set; }
+            public string Kategori { get; set; }
+            public string Beden { get; set; }   
+            public  double Fiyat { get; set; } 
+        }
+    }
+}
